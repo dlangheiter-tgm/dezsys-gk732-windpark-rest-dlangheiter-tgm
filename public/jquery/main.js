86a5f9ad6cd46fc782fb2202c0be85b543b7f8d2
David Langheiter <david@langheiter.com>
2018-11-06 16:24:33 +0100

    Implemented basic authentication

--- public/jquery/main.js
+++ public/jquery/main.js
@@ -1,18 +1,22 @@
+const exceptions = ["windengineID", "timestamp"]
+
 $(document).ready(async function () {
     const data = await $.getJSON("/windengine/001/data/json")
     const table = $('#table')
 
+    let units = {}
+
     for(i in data) {
-        table.append(tr([i, data[i]]))
+        if(exceptions.includes(i)) {
+            $("#" + i).text(data[i])
+        } else if(i.startsWith("unit")) {
+            units[i.replace("unit", "").toLowerCase()] = data[i]
+        } else {
+            table.append(`<tr><td>${i}</td><td id="${i}">${data[i]}</td></tr>`)
+        }
+    }
+
+    for(i in units) {
+        $(`#${i}`).append(" " + units[i])
     }
 })
-
-function tr(childs) {
-    let ret = "<tr>"
-    for(x of childs) {
-        ret += "<td>"
-        ret += x
-        ret += "</td>"
-    }
-    return ret + "</tr>"
-}
--- src/main/java/windpark/windengine/WindengineController.java
+++ src/main/java/windpark/windengine/WindengineController.java
@@ -1,43 +1,100 @@
 package windpark.windengine;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import windpark.model.WindengineData;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class WindengineController {
+    private List<String> tokens;
+
+    WindengineController() {
+        tokens = new ArrayList<>();
+    }
 
     @Autowired
     private WindengineService service;
 
-    // Not needed anymore because of public folder
-    /*@RequestMapping("/")
-    public String windengineMain() {
-        String mainPage = "This is the windengine application! (DEZSYS_GK72_WINDPARK) <br/><br/>" +
-                "<a href='http://localhost:8080/windengine/001/data/xml'>Link to windengine/001/data/xml</a><br/>" +
-                "<a href='http://localhost:8080/windengine/001/data/json'>Link to windengine/001/data/json</a><br/>" +
+    @RequestMapping("/windengine")
+    public ResponseEntity getWindengine(@CookieValue("auth") String token) {
+        String ret = "This is the windengine application! (DEZSYS_GK72_WINDPARK) <br/><br/>\n" +
+                "<a href='http://localhost:8080/jquery/index.html'>Link to windengine/001/jquery/index.html</a><br/>\n" +
+                "<a href='http://localhost:8080/windengine/001/data/xml'>Link to windengine/001/data/xml</a><br/>\n" +
+                "<a href='http://localhost:8080/windengine/001/data/json'>Link to windengine/001/data/json</a><br/>\n" +
                 "<a href='http://localhost:8080/windengine/001/transfer'>Link to windengine/001/transfer</a><br/>";
-        return mainPage;
-    }*/
+
+        return checkToken(token, ret);
+    }
+
+    @RequestMapping("/login")
+    public String login(
+            HttpServletResponse response,
+            @RequestParam(value = "user") String user,
+            @RequestParam(value = "password") String password) {
+
+        if (!user.equals(password)) {
+            try {
+                response.sendRedirect("/");
+            } catch (IOException e) {
+                return "Not authorized";
+            }
+        }
+
+        String uuid = UUID.randomUUID().toString();
+        tokens.add(uuid);
+        response.addCookie(new Cookie("auth", uuid));
+        try {
+            response.sendRedirect("/windengine");
+        } catch (IOException e) {
+            return "Not authorized";
+        }
+
+        return "Redirecting to <a href='/windengine'>Windengine</a>";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletResponse response, @CookieValue("auth") String token) {
+        tokens.remove(token);
+
+        try {
+            response.sendRedirect("/");
+        } catch (IOException e) {
+            return "Redirecting to <a href='/'>Home</a>";
+        }
+        return "";
+    }
+
+    private ResponseEntity checkToken(String token, Object object) {
+        if(tokens.contains(token)) {
+            return ResponseEntity.ok(object);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
 
     @RequestMapping(value = "/windengine/{windengineID}/data/xml", produces = "application/xml")
-    public WindengineData windengineDataXml(@PathVariable String windengineID) {
-        return service.getWindengineData(windengineID);
+    public ResponseEntity windengineDataXml(@PathVariable String windengineID, @CookieValue("auth") String token) {
+        return checkToken(token, service.getWindengineData(windengineID));
     }
 
     @RequestMapping(value = "/windengine/{windengineID}/data/json", produces = "application/json")
-    public WindengineData windengineDataJson(@PathVariable String windengineID) {
-        return service.getWindengineData(windengineID);
+    public ResponseEntity windengineDataJson(@PathVariable String windengineID, @CookieValue("auth") String token) {
+        return checkToken(token, service.getWindengineData(windengineID));
     }
 
     @RequestMapping("/windengine/{windengineID}/transfer")
-    public String windengineTransfer(@PathVariable String windengineID) {
-        return service.getGreetings("Windengine.Transfer!");
+    public ResponseEntity windengineTransfer(@PathVariable String windengineID, @CookieValue("auth") String token) {
+        return checkToken(token, service.getGreetings("Windengine.Transfer!"));
     }
-
 
 }
